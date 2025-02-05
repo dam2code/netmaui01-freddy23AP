40314fe7ea996022865e7280f0d46bb381aed6d6
freddy15t <99274453+freddy15t@users.noreply.github.com>
2025-02-05 17:48:20 +0100

    Phoneword1

---
+++ Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    string translatedNumber;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+
+        if (string.IsNullOrWhiteSpace(enteredNumber))
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+            return;
+        }
+
+        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            "Would you like to call " + translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(translatedNumber);
+                else
+                    await DisplayAlert("Not Supported", "Phone dialing is not supported on this device.", "OK");
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Not Supported", "Phone dialing is not supported on this device.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to dial", $"Phone dialing failed: {ex.Message}", "OK");
+            }
+        }
+    }
+}
