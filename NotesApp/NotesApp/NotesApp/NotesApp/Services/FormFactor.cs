ad5803312a742cca0903237f4b6ebb535a68a196
freddy15t <99274453+freddy15t@users.noreply.github.com>
2025-02-10 13:28:39 +0100

    NotesApp

---
+++ NotesApp/NotesApp/NotesApp/NotesApp/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using NotesApp.Shared.Services;
+
+namespace NotesApp.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return DeviceInfo.Idiom.ToString();
+        }
+
+        public string GetPlatform()
+        {
+            return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+        }
+    }
+}
