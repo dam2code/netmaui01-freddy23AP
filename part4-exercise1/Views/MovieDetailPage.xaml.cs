048b83637ba294d5db23fb5bec570ba8133754da
freddy15t <99274453+freddy15t@users.noreply.github.com>
2025-02-16 23:42:00 +0100

    excercise9/MovieCatalog1

---
+++ part4-exercise1/Views/MovieDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+using MovieCatalog.ViewModels;
+
+namespace MovieCatalog.Views;
+
+public partial class MovieDetailPage : ContentPage
+{
+    public MovieDetailPage()
+    {
+        BindingContext = App.MainViewModel.SelectedMovie;
+        InitializeComponent();
+    }
+}
