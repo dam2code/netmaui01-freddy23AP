d2bb5f005a6c1b0cf56d37bb2242fbd4c91efdbd
freddy15t <99274453+freddy15t@users.noreply.github.com>
2025-02-16 23:56:06 +0100

    excercise9/MovieCatalog2

---
+++ part6-exercise2/ViewModels/MovieListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MovieCatalog.ViewModels;
+
+public class MovieListViewModel : ObservableObject
+{
+    private MovieViewModel? _selectedMovie;
+
+    public MovieViewModel? SelectedMovie
+    {
+        get => _selectedMovie;
+        set => SetProperty(ref _selectedMovie, value);
+    }
+
+    public ObservableCollection<MovieViewModel> Movies { get; set; }
+
+    public ICommand DeleteMovieCommand { get; private set; }  // Nuevo comando
+
+    public MovieListViewModel()
+    {
+        Movies = new ObservableCollection<MovieViewModel>();
+        DeleteMovieCommand = new Command<MovieViewModel>(DeleteMovie); // Asignar el comando
+    }
+
+    public async Task RefreshMovies()
+    {
+        IEnumerable<Models.Movie> moviesData = await Models.MoviesDatabase.GetMovies();
+
+        foreach (Models.Movie movie in moviesData)
+            Movies.Add(new MovieViewModel(movie));
+    }
+
+    public void DeleteMovie(MovieViewModel movie)
+    {
+        if (Movies.Contains(movie))
+            Movies.Remove(movie);
+    }
+}
---
+++ part6-exercise2/Views/MoviesListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;  // Asegurar que ContentPage y Navigation estén disponibles
+using MovieCatalog.ViewModels;   // Para MovieViewModel
+using System.Windows.Input;
+
+namespace MovieCatalog.Views;
+
+public partial class MoviesListPage : ContentPage
+{
+    public MoviesListPage()
+    {
+        InitializeComponent();
+        BindingContext = App.MainViewModel;  // Asegurar el contexto de datos
+    }
+
+    private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        if (e.Item is MovieViewModel selectedMovie)
+        {
+            await Navigation.PushAsync(new MovieDetailPage(selectedMovie)); // Pasar la película seleccionada
+        }
+    }
+}
